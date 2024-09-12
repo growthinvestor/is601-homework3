c1e0dd7914d193d913a054ce1b9c429473ca6f97
growthinvestor <amanjaswal007@gmail.com>
2024-09-12 09:19:14 -0400

    updated files

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,10 +2,11 @@
 from calculator import add, subtract
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    '''Test that addition function works '''
+    assert add(2, 2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    '''Test that subtraction function works '''
+    assert subtract(2, 2) == 0
+
 
